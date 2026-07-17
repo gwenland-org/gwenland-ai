fff304ba58291a6acc460f674efbe1ba04565472
JinXSuperSolo <jinxsuperdev@gmail.com>
2026-07-17 11:31:43 +0700

    feat: create glictus-caliburni boilerplate and bump version to 0.1.148

---
+++ glictus-caliburni/src/loader.rs
@@ -0,0 +1,5 @@
+/// TODO: Implement LazyLoader structure
+#[derive(Debug, Clone)]
+pub struct LazyLoader {
+    // TODO: Add fields for lazy loading of shards/weights
+}
---
+++ glictus-caliburni/src/shard.rs
@@ -0,0 +1,5 @@
+/// TODO: Implement ShardMap structure
+#[derive(Debug, Clone)]
+pub struct ShardMap {
+    // TODO: Add fields for mapping shards to experts
+}
